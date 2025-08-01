4423352bfff9d2e17976936fa5548541588b3b8d
HarryT63 <harry.tomasco@gmail.com>
2025-07-31 21:58:19 -0400

    Created a temporary start screen, added a gravity radius to the goal, imported doTween

---
+++ OutOfTheLoop/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+
+
+
+public class SceneManager : MonoBehaviour
+{
+    public string SceneName;
+    public Button startButton;
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(OnButtonPressed);
+
+    }
+
+    private void OnButtonPressed()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(SceneName);
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(SceneName);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        }
+    }
+}
