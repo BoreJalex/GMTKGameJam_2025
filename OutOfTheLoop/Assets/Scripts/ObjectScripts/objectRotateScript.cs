55022951e2b7ce91afa5c9d6c120289cdb3e5e1a
HarryT63 <harry.tomasco@gmail.com>
2025-08-02 17:57:29 -0400

    imported drawings made this morning and added rotation script that makes planets and other items rotate slowly

---
+++ OutOfTheLoop/Assets/Scripts/ObjectScripts/objectRotateScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectRotateScript : MonoBehaviour
+{
+public float rotationSpeed = 50f; // Speed of rotation in degrees per second
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        // Rotate the object around its own axis
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ OutOfTheLoop/Assets/Scripts/ObjectScripts/wormholeScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wormholeScript : MonoBehaviour
+{
+
+    public GameObject otherPoint;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if the object that entered the trigger is the player
+        if (collision.CompareTag("Player"))
+        {
+            // Move the player to the other wormhole point
+            collision.transform.position = otherPoint.transform.position;
+            StartCoroutine(Cooldown());
+        }
+    }
+
+    IEnumerator Cooldown()
+    {
+        // Disable the wormhole for a short duration to prevent immediate re-entry
+        gameObject.GetComponent<CircleCollider2D>().enabled = false;
+        otherPoint.GetComponent<CircleCollider2D>().enabled = false;
+        yield return new WaitForSeconds(.1f); // Adjust the cooldown duration as needed
+        gameObject.GetComponent<CircleCollider2D>().enabled = true;
+        otherPoint.GetComponent<CircleCollider2D>().enabled = true;
+        Debug.Log("Wormhole activated, cooldown complete.");
+
+    }
+
+}
